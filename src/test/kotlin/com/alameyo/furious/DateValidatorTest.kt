0d79a90dfaf1f05ab2ec3eb5b33ccf0895651d30
Alameyo <Alameyo@users.noreply.github.com>
2021-11-13 22:46:54 +0100

    date validation positive case

--- src/test/kotlin/com/alameyo/furious/DateValidatorTest.kt
+++ src/test/kotlin/com/alameyo/furious/DateValidatorTest.kt
@@ -4,6 +4,7 @@ import com.alameyo.furiouscinema.inputvalidation.DateValidator
 import com.alameyo.furiouscinema.inputvalidation.InputValidationException
 import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
 
 class DateValidatorTest {
     private val dateValidator = DateValidator()
@@ -12,6 +13,12 @@ class DateValidatorTest {
     fun `If input have invalid date - should throw InputValidationException`() {
         val invalidDateJson = """20-11-11""".trim()
         assertThrows(InputValidationException::class.java) { dateValidator.validate(invalidDateJson) }
+    }
+
+    @Test
+    fun `If input have valid date - should not throw exception`() {
+        val invalidDateJson = """2021-11-11""".trim()
+        assertDoesNotThrow { dateValidator.validate(invalidDateJson) }
     }
 
 }
