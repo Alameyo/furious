1fa399c584003404ff9dfff06b433688b38d88dd
Alameyo <Alameyo@users.noreply.github.com>
2021-11-13 16:01:00 +0100

    detekt issues in MovieReviewValidatorTest

--- src/test/kotlin/MovieReviewValidatorTest.kt
+++ src/test/kotlin/MovieReviewValidatorTest.kt
@@ -9,50 +9,50 @@ class MovieReviewValidatorTest {
 
     @Test
     fun `Valid review - should not throw exception`() {
-        val validReviewBody = """  
+        val validReviewBody = """
             {
                 "rate": 5,
                 "comment": "Great movie"
             }
-            """
+            """.trim()
         assertDoesNotThrow { movieReviewValidator.validate(validReviewBody) }
     }
 
     @Test
     fun `Rate too high - should throw exception`() {
-        val highRateInput = """  
+        val highRateInput = """
             {
                 "rate": 6,
                 "comment": "Great movie"
             }
-            """
+            """.trim()
         assertThrows(InputValidationException::class.java) { movieReviewValidator.validate(highRateInput) }
     }
 
     @Test
     fun `Rate too low - should throw exception`() {
-        val lowRateInput = """  
+        val lowRateInput = """
             {
                 "rate": 0,
                 "comment": "Great movie"
             }
-            """
+            """.trim()
         assertThrows(InputValidationException::class.java) { movieReviewValidator.validate(lowRateInput) }
     }
 
     @Test
     fun `Comment doesn't exist - should not throw exception`() {
-        val lowRateInput = """  
+        val lowRateInput = """
             {
                 "rate": 5
             }
-            """
+            """.trim()
         assertDoesNotThrow { movieReviewValidator.validate(lowRateInput) }
     }
 
     @Test
     fun `Json not formatted properly - should throw exception`() {
-        val invalidJson = """  
+        val invalidJson = """
             {
                 "rate": 2,,,,,,,:,,,,
                 "comment": "Great movie"
@@ -64,7 +64,7 @@ class MovieReviewValidatorTest {
     @Test
     @Suppress("MaxLineLength")
     fun `Too long comment - should throw exception`() {
-        val invalidJson = """  
+        val invalidJson = """
             {
                 "rate": 2,
                 "comment": "Great movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movieGreat movie"
