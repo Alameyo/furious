73d9d76e1760cc64cd65323dee269c8ccbbb2636
Alameyo <Alameyo@users.noreply.github.com>
2021-11-13 22:45:07 +0100

    static import

--- src/test/kotlin/com/alameyo/furious/DateValidatorTest.kt
+++ src/test/kotlin/com/alameyo/furious/DateValidatorTest.kt
@@ -2,7 +2,7 @@ package com.alameyo.furious
 
 import com.alameyo.furiouscinema.inputvalidation.DateValidator
 import com.alameyo.furiouscinema.inputvalidation.InputValidationException
-import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.api.Test
 
 class DateValidatorTest {
@@ -11,7 +11,7 @@ class DateValidatorTest {
     @Test
     fun `If input have invalid date - should throw InputValidationException`() {
         val invalidDateJson = """20-11-11""".trim()
-        Assertions.assertThrows(InputValidationException::class.java) { dateValidator.validate(invalidDateJson) }
+        assertThrows(InputValidationException::class.java) { dateValidator.validate(invalidDateJson) }
     }
 
 }
